344dd0c8dbf1112642e19e6000a040e955765e6a
oheyek <oheyek@proton.me>
2025-08-20 16:33:07 +0200

    Implement argument parsing for CLI operations

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import sys
+import argparse
+
 
 def main() -> None:
     """
     Main function of the program.
     """
-    print("CLI Test.")
+    parser = argparse.ArgumentParser(description="Test for expense tracker arg parsing.")
+    parser.add_argument("operation", type=str, help="Test operation")
+    arg = parser.parse_args()
+    print(arg.operation)
+    if arg.operation == "test":
+        print("CLI Test.")
 
 if __name__ == "__main__":
     main()
