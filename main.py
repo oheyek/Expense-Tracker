2b89bd7d8bd1d281660c1afe80afeb4c1387e01b
oheyek <oheyek@proton.me>
2025-08-20 17:02:51 +0200

    Implement expense operations in CLI

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import argparse
 
+def add_expenses():
+    print("Add operation working.")
+
+def list_expenses():
+    print("List operation working.")
+
+def summarize_expenses():
+    print("Summary operation working.")
+
+def delete_expense():
+    print("Delete operation working.")
 
 def main() -> None:
-    """
-    Main function of the program.
-    """
-    parser = argparse.ArgumentParser(description="Test for expense tracker arg parsing.")
-    parser.add_argument("operation", type=str, help="Test operation")
-    arg = parser.parse_args()
-    print(arg.operation)
-    if arg.operation == "test":
-        print("CLI Test.")
+    parser = argparse.ArgumentParser(description="Operation argument for expense tracker.")
+    parser.add_argument("operation", type=str, help="Operation to perform.")
+    operation = parser.parse_args().operation
+
+    match operation:
+        case "add":
+            add_expenses()
+        case "list":
+            list_expenses()
+        case "summary":
+            summarize_expenses()
+        case "delete":
+            delete_expense()
+        case _:
+            raise ValueError("Invalid operation.")
 
 if __name__ == "__main__":
     main()
