5d1c4623c6e4e6fa154f3c88398a36f16b5f0f52
oheyek <oheyek@proton.me>
2025-08-20 17:39:48 +0200

    Enhance CLI to support adding expenses with description and amount

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import argparse
 
-def add_expenses():
-    print("Add operation working.")
+
+def add_expenses(args):
+    description = args.description
+    amount = args.amount
+    print(description, amount)
 
 def list_expenses():
     print("List operation working.")
 
+
 def summarize_expenses():
     print("Summary operation working.")
+
 
 def delete_expense():
     print("Delete operation working.")
 
+
 def main() -> None:
+    """
+    Main function of the program.
+    """
     parser = argparse.ArgumentParser(description="Operation argument for expense tracker.")
-    parser.add_argument("operation", type=str, help="Operation to perform.")
-    operation = parser.parse_args().operation
+    subparsers = parser.add_subparsers(dest="operation", required=True)
+
+    # Adding an expense
+    add_parser = subparsers.add_parser("add", help="Add a new expense")
+    add_parser.add_argument("--description", type=str, required=True, help="Description of the expense")
+    add_parser.add_argument("--amount", type=float, required=True, help="The amount of the expense")
+
+
+    args = parser.parse_args()
+    operation = args.operation
 
     match operation:
         case "add":
-            add_expenses()
+            add_expenses(args)
         case "list":
             list_expenses()
         case "summary":
@@ -28,6 +45,7 @@ def main() -> None:
             delete_expense()
         case _:
             raise ValueError("Invalid operation.")
+
 
 if __name__ == "__main__":
     main()
